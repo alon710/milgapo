8b5333310d71471da413b6bdac0687912d20c653
Alon Barad <alon710@gmail.com>
2025-03-01 21:32:52 +0200

    Remove unused tutorial components and update dashboard redirection logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,30 @@
-import Hero from "@/components/hero";
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+import { encodedRedirect } from "@/utils/utils";
+import { signInAction } from "@/app/actions";
+import { FormMessage, Message } from "@/components/form-message";
+import { SubmitButton } from "@/components/submit-button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { createClient } from "@/utils/supabase/server";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
 
 export default async function Home() {
+    const supabase = await createClient();
+  
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+  
+    if (user) {
+      return redirect("/dashboard");
+    } else {
+      return redirect("/sign-in");
+    }
+
   return (
     <>
-      <Hero />
-      <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-      </main>
+      <div></div>
     </>
   );
 }
