ec0c057b81874a0c4e4cb3c6d465349347b674a1
Alon Uri Barad <Alon710@gmail.com>
2025-03-04 12:40:45 +0200

    Simplify Home component by removing user authentication checks and adding a header

--- app/page.tsx
+++ app/page.tsx
@@ -2,21 +2,11 @@ import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
-  const supabase = await createClient();
-
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
-  if (user) {
-    return redirect("/dashboard");
-  } else {
-    return redirect("/login");
-  }
-
   return (
     <>
-      <div></div>
+      <div>
+        <h1>Home</h1>
+      </div>
     </>
   );
 }
