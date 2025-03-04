97674cffd7039762ebe0a54ef47e9a0cee79f65b
Alon Uri Barad <Alon710@gmail.com>
2025-03-04 14:21:31 +0200

    Enhance user greeting in AuthButton component to display name, email, or phone

--- components/header-auth.tsx
+++ components/header-auth.tsx
@@ -3,15 +3,29 @@ import Link from "next/link";
 import { Button } from "./ui/button";
 import { createClient } from "@/utils/supabase/server";
 import { authConfig } from "@/config/auth";
+import { User } from "@supabase/supabase-js";
+
 export default async function AuthButton() {
     const supabase = await createClient();
+    const getUserGreetingName = (user: User) => {
+        if (user?.user_metadata?.name) {
+            return user.user_metadata.name;
+        }
+        if (user.email) {
+            return user.email;
+        }
+        if (user.phone) {
+            return user.phone;
+        }
+    };
 
     const {
         data: { user }
     } = await supabase.auth.getUser();
+
     return user ? (
         <div className="flex items-center gap-4">
-            {authConfig.greeting}, {user.email ? user.email : user.phone}!
+            {authConfig.greeting} {getUserGreetingName(user)}
             <form action={signOutAction}>
                 <Button type="submit" variant={"default"} size="sm">
                     {authConfig.logoutButtonText}
