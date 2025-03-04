c7f72147e1a4edfdbeaa3795c24b9dadaa896600
Alon Uri Barad <Alon710@gmail.com>
2025-03-04 11:27:46 +0200

    Refactor layout and authentication components; remove environment variable checks and update site metadata for Hebrew support

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,18 @@
-import { EnvVarWarning } from "@/components/env-var-warning";
 import HeaderAuth from "@/components/header-auth";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 import { Geist } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { SiteLogo } from "@/components/layout/site-logo";
 import { SiteFooter } from "@/components/layout/footer";
-
+import { siteConfig, SiteConfig } from "@/config/site";
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : "http://localhost:3000";
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Milgapo",
-  description: "Milgapo your home for scholarships",
+  title: siteConfig.titleHebrew,
+  description: siteConfig.description,
 };
 
 const geistSans = Geist({
@@ -40,7 +38,7 @@ export default function RootLayout({
             <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
               <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                 <SiteLogo href="/" />
-                {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
+                <HeaderAuth />
               </div>
             </nav>
             <div className="flex flex-col gap-20 max-w-5xl p-5">
--- config/site.ts
+++ config/site.ts
@@ -1,6 +1,7 @@
 export const siteConfig = {
   titleEnglish: "Milgapo",
   titleHebrew: "מלגפו",
+  description: "Milgapo is a platform for learning Hebrew",
 };
 
 export type SiteConfig = typeof siteConfig;
