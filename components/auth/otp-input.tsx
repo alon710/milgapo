47251f61bda928438203fc43052e21566fe5c725
Alon Barad <alon710@gmail.com>
2025-03-06 16:53:25 +0200

    Fix whitespace formatting in OTP input component

--- components/auth/otp-input.tsx
+++ components/auth/otp-input.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useRef,useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import { Input } from "@/components/ui/input";
 
