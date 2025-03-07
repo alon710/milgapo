c4fb35e1801a2dbc09018ab53514a686fa62a5d2
Alon Barad <alon710@gmail.com>
2025-03-07 17:47:41 +0200

    Remove Suspense wrapper from OTP page

--- app/(auth)/otp/page.tsx
+++ app/(auth)/otp/page.tsx
@@ -1,19 +1,5 @@
-import React, { Suspense } from "react";
-
 import OtpForm from "@/components/auth/otp-form";
 
-function OtpFormLoader() {
-    return (
-        <div className="flex items-center justify-center min-h-[300px]">
-            <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-primary"></div>
-        </div>
-    );
-}
-
 export default function OtpPage() {
-    return (
-        <Suspense fallback={<OtpFormLoader />}>
-            <OtpForm />
-        </Suspense>
-    );
+    return <OtpForm />;
 }
